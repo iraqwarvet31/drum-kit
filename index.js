006018a8f859070dd00e33be022c6ab26743640c
Larry <48338639+iraqwarvet31@users.noreply.github.com>
2020-02-25 17:36:27 -0800

    Add initial drum kit website files

---
+++ index.js
@@ -0,0 +1,60 @@
+// Detecting Button Press
+
+let numberOfDrumButtons = document.querySelectorAll(".drum");
+
+for (let i = 0; i < numberOfDrumButtons.length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        let buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    });
+}
+
+// Detecting Key press
+document.addEventListener('keypress', function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+// Function that that plays specific sound
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'k':
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'l':
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector(`.${currentKey}`);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")
+    }, 100)
+}
